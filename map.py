f78ae7e66065b2cb4697f88cb5ad605ab2bede02
marbybr <b.meijer@tilburguniversity.edu>
2024-05-24 13:48:47 +0200

    Added a dropdown menu

--- map.py
+++ map.py
@@ -3,7 +3,7 @@
 #pip install dash-bootstrap-components
 #pip install pandas
 
-# from dash import Dash, html, dash_table, dcc, callback, Output, Input
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
@@ -15,39 +15,8 @@ import plotly.graph_objects as go
 #Load the dataset
 df = pd.read_csv('data_cleaned.csv')
 
-# # Get sample of the dataset
-# df_sample = df.sample(1, random_state=0, replace=False)
-
-# fig = go.Figure(data=
-#     go.Parcoords(
-#         line = dict(color = df['Gender'],
-#                    colorscale = [[0,'purple'],[1,'gold']]),
-#         dimensions = list([
-#             dict(range = [0,1],
-#                 constraintrange = [0,1],
-#                 label = 'Family history', values = df['family_history']),
-#             dict(range = [0,1],
-#                 label = 'Coping struggles', values = df['Coping_Struggles']),
-#             dict(range = [0,1],
-#                 label = 'Treatment', values = df['treatment'])
-#         ])
-#     )
-# )
-
-# fig.update_layout(
-#     plot_bgcolor = 'white',
-#     paper_bgcolor = 'white'
-# )
-
-# fig.show()
-# df_ = df.loc[:, [col for col in df.columns.values if col != "Timestamp"]]
-# df_ = df_.sample(10000, random_state=0)
-# df_tnse = TSNE(n_components=2, learning_rate='auto',
-#                init='random', perplexity=30, random_state=0).fit_transform(df_.to_numpy())
-# print(df_tnse.shape)
-# plt.figure()
-# plt.scatter(df_tnse[:, 0], df_tnse[:, 1])
-# plt.show()
+# Set default name for `Country column`
+COUNTRY_COL = "Country"
 
 def group_by_country(df: pd.DataFrame, col: str):
     """Groups a dataframe by country and selects a column."""
@@ -61,16 +30,57 @@ def group_by_country(df: pd.DataFrame, col: str):
     
     return inner
 
-data = group_by_country(df, 'treatment')()
+# Create app layout
+app = Dash()
+app.title = "Mental Health Dataset Analysis"
 
-# Load world map from Geopandas datasets
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+app.layout = html.Div([
 
-# Merge the dataframe with the world GeoDataFrame
-world = world.merge(data, how='left', left_on='name', right_on='Country')
-fig = px.choropleth(
-    world, locations='iso_a3', color='treatment',
-    hover_name='Country', hover_data=['Country', 'treatment', 'continent', 'pop_est'],
-    color_continuous_scale=px.colors.sequential.Viridis_r
+    # Dropdown menu to select column to be displayed in map
+    html.Div([
+        dcc.Dropdown(id='columns_dropdown',
+                     options=[column for column in df.columns if column not in ['Timestamp', "Country"]],
+                     value='treatment',
+                     style={'width':'50%', 'display': 'inline-block'}
+        ),        
+    ]),
+
+    html.Br(),
+
+    # Figure -- Map
+    dcc.Graph(id='percentages_map', clear_on_unhover=True, 
+              style={'width': '90vw', 'height': '80vh', 'display': 'inline-block'})
+])
+
+# Define callback function for updating map display
+@app.callback(
+    Output(component_id='percentages_map', component_property='figure'),
+    Input(component_id='columns_dropdown', component_property='value')
 )
-fig.show()    
+
+def update_percentages_map(dropdown_value):
+
+    data = group_by_country(df, dropdown_value)()
+
+    # Load world map from Geopandas datasets
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    # Merge the dataframe with the world GeoDataFrame
+    world = world.merge(data, how='left', left_on='name', right_on=COUNTRY_COL)
+
+    fig = px.choropleth(
+        world, locations='iso_a3', color=dropdown_value,
+        hover_name=COUNTRY_COL, hover_data=[COUNTRY_COL, dropdown_value, 'continent', 'pop_est'],
+        color_continuous_scale=px.colors.sequential.Viridis_r
+    )
+    fig.update_layout(
+        title=dict(text=f"{dropdown_value.title()} (%) per Country", 
+                   font=dict(size=30), automargin=True, yref='container',
+                   y=0.95)
+    ) # Inspired from https://plotly.com/python/figure-labels/
+
+    return fig
+
+# Run app
+if __name__ == '__main__':
+    app.run_server(debug=True)
