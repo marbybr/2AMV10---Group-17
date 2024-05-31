8b24564e86b27586fc2976548942a1eda519026b
Patrick Huijten <p.j.huijten@student.tue.nl>
2024-05-31 17:53:29 +0200

    Implemented Multi-Dropdown

--- app.py
+++ app.py
@@ -6,6 +6,7 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import dash_bootstrap_components as dbc #Used for creating more advanced layouts
 
 #Load the dataset
@@ -23,15 +24,15 @@ app.layout = dbc.Container([
     ]),
 
     #Construct the dropdown menu of all columns except for index and Timestamp, 
-    #set the 3nd column (so first after index and Timestamp) as the standard selected option
+    #set the 2nd column (so first after index and Timestamp) as the standard selected option
     dbc.Row([
-        dcc.Dropdown(df.columns[2:], df.columns[2], id='feature-dropdown'),
+        dcc.Dropdown(df.columns[1:], df.columns[1], id='feature-dropdown', multi=True),
         html.Div(id='feature-dropdown-container')
     ]),
 
     #Construct a Row (visually speaking) with 2 columns, 1 for the dataset, 1 for the plot
     dbc.Row([
-        #Show thethe Time, treatment and history features of the first 10 rows the the dataset (probably not needed in final product)
+        #Show the Time, treatment and history features of the first 10 rows the the dataset (probably not needed in final product)
         dbc.Col([
             dash_table.DataTable(data=df[['Timestamp', 'family_history', 'treatment']].to_dict('records'), page_size=10, id='table')
         ], width=6),
@@ -39,7 +40,12 @@ app.layout = dbc.Container([
         dbc.Col([
             dcc.Graph(figure={}, id='feature-distribution')
         ], width=6)
+    ]),
+    
+    dbc.Row([
+        dcc.Dropdown([f"{col}{sign}" for col in df.columns[1:] for sign in [' = True', ' = False']], id='filter', multi=True)
     ])
+
 ])
 
 #Builds interaction between the displayed text below the dropdown menu and the graph
@@ -49,9 +55,61 @@ app.layout = dbc.Container([
     Input(component_id='feature-dropdown', component_property='value'),
 )
 
-def update_values(selected_feature):
-    printed_text = 'You have selected {}'.format(selected_feature)
-    fig = px.histogram(df, x=selected_feature)
+def update_values(selected_features):
+
+    #Generate the text message that shows which features were selected
+    if type(selected_features) == str:
+        selected_features = [selected_features]
+    printed_text = 'You have selected the following feature(s): {}'.format(', '.join(selected_features))
+
+    #Create a filtered dataset based on the 2nd dropdown and make the figure, model etc. with that filtered dataset
+
+
+
+
+
+
+
+
+
+
+    #If no features were selected, return an empty figure
+    if len(selected_features) == 0:
+        fig = go.Figure()
+        return printed_text, fig
+
+    #Construct the updated barchart
+    count_data = df[selected_features].apply(lambda x: x.value_counts(normalize=True)).T
+    count_data.columns = ['count_0', 'count_1']
+    count_data.reset_index(inplace=True)
+    count_data.rename(columns={'index': 'variable'}, inplace=True)
+
+    fig = go.Figure()
+
+    for _, row in count_data.iterrows():
+        fig.add_trace(go.Bar(
+            x=[row['variable']],
+            y=[row['count_0']],
+            name=f"{row['variable']} = 0",
+            marker_color='blue'
+            ))
+        fig.add_trace(go.Bar(
+            x=[row['variable']],
+            y=[row['count_1']],
+            name=f"{row['variable']} = 1",
+            marker_color='orange'
+            ))
+    
+    # Update layout
+    fig.update_layout(
+        barmode='group',
+        title="Counts of Binary Variables",
+        xaxis_title="Variable",
+        yaxis_title="Ratio",
+        legend_title="Value"
+    )
+    fig.update_yaxes(range=[0, 1])
+
     return printed_text, fig
 
 
