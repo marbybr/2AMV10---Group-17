838f8ee7b97238618af5c50edeefdf7da208ca4b
Patrick Huijten <p.j.huijten@student.tue.nl>
2024-06-18 15:21:22 +0200

    Improved layout and fixed some bugs

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ app.layout = dbc.Container([
     dbc.Row([
         #Show the Time, treatment and history features of the first 10 rows the the dataset (probably not needed in final product)
         dbc.Col([
+<<<<<<< Updated upstream
             html.Div(style={'height': '20px'}),
             html.Div('Select features you would like to view in the table in the dropdown menu below'),
             dcc.Dropdown(df.columns, ['Timestamp', 'family_history', 'treatment'], id='table-feature-selection', multi=True),
@@ -47,6 +48,19 @@ app.layout = dbc.Container([
         dbc.Col([
             dcc.Graph(figure={}, id='feature-distribution')
         ], width=7)
+=======
+            #html.Br(),
+            dcc.Graph(id='percentages_map', clear_on_unhover=True, style={'height': '70vh'})
+            ], width=7),  # Map  
+
+        dbc.Col([
+            dcc.Graph(id='country_barplot', clear_on_unhover=True, style={'height': '50vh'})
+            ], width=2),  # Histogram
+        #Show a simple histogram for the selected feature
+        dbc.Col([
+            dcc.Graph(figure={}, id='feature-distribution')
+            ], width=3)
+>>>>>>> Stashed changes
     ]),
 ])
 
@@ -102,6 +116,7 @@ def update_values(table_features, selected_features, filters):
         fig.add_trace(go.Bar(
             x=[row['variable']],
             y=[row['count_0']],
+<<<<<<< Updated upstream
             name="False",
             marker_color='red',
             showlegend = legend_check,
@@ -116,24 +131,225 @@ def update_values(table_features, selected_features, filters):
             width=0.25
             ))
         legend_check = False
+=======
+            #name=f"{row['variable']} = 0",
+            marker_color='red',
+            showlegend=False
+        ))
+        fig.add_trace(go.Bar(
+            x=[row['variable']],
+            y=[row['count_1']],
+            #name=f"{row['variable']} = 1",
+            marker_color='blue',
+            showlegend=False
+        ))
+>>>>>>> Stashed changes
+    
+    fig.add_trace(go.Bar(
+        x=[None],  # Dummy value
+        y=[None],  # Dummy value
+        name="= False",
+        marker_color='red',
+    ))
+    fig.add_trace(go.Bar(
+        x=[None],  # Dummy value
+        y=[None],  # Dummy value
+        name="= True",
+        marker_color='blue'
+    ))
     
     # Update layout
     fig.update_layout(
         barmode='stack',
+<<<<<<< Updated upstream
         title="Counts of Binary Variables",
+=======
+        title=dict(
+            text="Distributions of selected features",
+            font=dict(size=15), 
+            automargin=True, 
+            yref='container',
+            y=0.95
+            ),
+>>>>>>> Stashed changes
         xaxis_title="Variable",
         yaxis_title="Ratio",
         legend_title="Value",
         xaxis_tickangle = -45,
         height = 600,
         bargap = 0.5, #Space between groups
+<<<<<<< Updated upstream
         bargroupgap = 0.5 #Space within groups
+=======
+        bargroupgap = 0.5, #Space within groups
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)',
+        font=dict(color='white',
+                  size=8)
+>>>>>>> Stashed changes
     )
     fig.update_yaxes(range=[0, 1])
 
     table = df_filtered[table_features].to_dict('records')
 
+<<<<<<< Updated upstream
     return printed_text, fig, table
+=======
+    # Load world map from Geopandas datasets
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    # Merge the dataframe with the world GeoDataFrame
+    world = world.merge(data, how='left', left_on='name', right_on=COUNTRY_COL)
+    
+    min_dropdown_value = find_closest(world[dropdown_value].min(), True)
+    max_dropdown_value = find_closest(world[dropdown_value].max(), False)
+
+    fig2 = px.choropleth(
+        world, locations='iso_a3', color=dropdown_value,
+        hover_name=COUNTRY_COL, hover_data=[COUNTRY_COL, dropdown_value, 'continent', 'pop_est'],
+        color_continuous_scale=px.colors.sequential.Viridis_r,
+        range_color=(min_dropdown_value, max_dropdown_value)
+    )
+
+    fig2.update_layout(
+        title=dict(text=f"{dropdown_value.title()} (%) per Country",
+                   font=dict(size=20), automargin=True, yref='container',
+                   y=0.95),
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)',
+        font=dict(color='white'),
+        coloraxis_colorbar=dict(
+            title=dict(font=dict(size=25)),
+            tickfont=dict(size=20),
+            tickformat=".0%"
+        )
+    )
+
+    #Now we update the histogram associated with the worldmap plot
+    country = "United States of America"
+    if hoverDataMap:
+        country = hoverDataMap['points'][0]['customdata'][0]
+    data = df_filtered.loc[df_filtered[COUNTRY_COL].isin([country]), :]
+
+    fig3 = px.histogram(
+        data_frame=data, 
+        x=dropdown_value_hist, 
+        color=dropdown_value_hist,
+        hover_name=COUNTRY_COL, 
+        hover_data=[COUNTRY_COL, dropdown_value_hist],
+        color_discrete_map={True: 'blue', False: 'red'}
+    )
+    fig3.update_layout(
+        title=dict(
+            text=f"{dropdown_value_hist} in {country}",
+            font=dict(size=10), 
+            automargin=True, 
+            yref='container',
+            y=0.95
+        ), 
+        xaxis_title=dict(
+            text=f"{dropdown_value_hist}",
+            font=dict(size=16)
+        ),
+        yaxis_title=dict(
+            text='Count',
+            font=dict(size=16)
+        ),
+        legend_title=dict(
+            text=f"{dropdown_value_hist}",
+            font=dict(size=16)
+        ),
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)',
+        font=dict(color='white'),
+        xaxis=dict(
+            showgrid=False,
+            zeroline=False,
+            showline=True,
+            linecolor='white'
+        ),
+        yaxis=dict(
+            showgrid=True,
+            gridcolor='white',
+            zeroline=False,
+            showline=True,
+            linecolor='white'
+        )
+    )
+
+    #### 
+    if selected_n_clicks is None and full_n_clicks is None:
+        # return ""
+        ###new
+        feature_importances_fig = px.bar(height = 250)
+        fig_cf = go.Figure()
+        return fig, fig2, fig3, feature_importances_fig, fig_cf
+    
+    # df for training
+    # df_train = df_filtered[selected_features]
+    if selected_n_clicks:
+        df_train = df_filtered[selected_features]
+    elif full_n_clicks:
+        df_train = df_filtered.drop(['Country', 'Timestamp'], axis=1)
+
+    target = 'treatment'  
+    X, clf, X_test, y_test = train(df_filtered.drop(['Country', 'Timestamp'], axis=1).astype(int), 
+                                   df_train.astype(int), target, selected_features)
+
+    #feature importance
+    importances = clf.coef_[0]
+    feature_importances = pd.Series(importances, index=X.columns)
+    feature_importances = feature_importances.sort_values(ascending=True)
+    #feature_importances = feature_importances[feature_importances >= 0]
+
+    feature_importances_fig = px.bar(feature_importances, x=feature_importances.values, y=feature_importances.index,
+                                     labels={'x': 'Importance', 'y': 'Feature'},
+                                     title='Feature Importances',
+                                     height = 250)
+
+    feature_importances_fig.update_layout(
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)',
+        font=dict(color='white')
+    )
+
+    # Lastly, the counterfactuals
+    if isinstance(cf_features, str):
+        cf_features = [cf_features]
+    elif cf_features is None:
+        fig_cf = go.Figure()
+    
+    try:
+        features_to_vary = [feature for feature in cf_features if feature in mutable_features]
+        print(features_to_vary)
+        cf, differences = get_counterfactuals_from_model(X=X_test, y=y_test, model=clf, features_to_vary=features_to_vary, 
+                                                        outcome_name=target, idx=list(range(3)), total_CFs=15)
+        print(differences)
+        
+        # For now get first row
+        sample_row = differences[0][0]
+
+        # Make plot of data
+        plot_data = pd.DataFrame({
+        'Feature': sample_row.columns,
+        'Value': sample_row.values.flatten()
+        })
+        plot_data['Color'] = plot_data['Value'].map({-1: 'red', 1: 'blue'})
+
+        # Create the horizontal bar plot
+        fig_cf = px.bar(plot_data, x='Value', y='Feature', orientation='h', color='Color',
+                    title='Feature Values', labels={'Value': 'Value', 'Feature': 'Feature'},
+                    color_discrete_map={'red': 'red', 'blue': 'blue'})
+        # Update legend title and labels
+        fig_cf.update_layout(
+            legend_title_text='What to change'
+        )
+        fig_cf.for_each_trace(lambda t: t.update(name={'red': '1 --> 0', 'blue': '0 --> 1'}[t.name]))
+    except:
+        fig_cf = go.Figure()
+
+    return fig, fig2, fig3, feature_importances_fig, fig_cf
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
